519998d92da51a2fa0784b8740393c477e842ac8
OlivierArgentieri <olivierargentieri@gmail.com>
2023-09-06 22:32:16 -0400

    ok first test ap from template

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -1,14 +1,30 @@
-#include <stdio.h>
 #include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_mac.h"
 #include "esp_wifi.h"
-
+#include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
+
 #include "lwip/err.h"
 #include "lwip/sys.h"
 
 
-static const char *TAG = "wifi softAP";
+static const char *TAG = "wifi softAP";\
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                    int32_t event_id, void* event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
+        ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
 void init_log(void)
 {
   /*
@@ -19,6 +35,8 @@ void init_log(void)
    * ESP_LOGD (debug)
    * ESP_LOGV (verbose)
    */
+
+  // print message to the console as info
   ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
 }
 
@@ -38,9 +56,53 @@ void init_nvs(void)
   }
 }
 
+void init_soft_ap(void)
+{
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  esp_netif_create_default_wifi_ap();
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+    WIFI_EVENT,
+    ESP_EVENT_ANY_ID,
+    &wifi_event_handler,
+    NULL,
+    NULL)
+  );
+
+  wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = "aaaaaaa",
+            .ssid_len = strlen("aaaaaaa"),
+            .channel = 1,
+            .password = "",
+            .max_connection = 4,
+#ifdef CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT
+            .authmode = WIFI_AUTH_WPA3_PSK,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+#else /* CONFIG_ESP_WIFI_SOFTAP_SAE_SUPPORT */
+            .authmode = WIFI_AUTH_WPA2_PSK,
+#endif
+            .pmf_cfg = {
+                    .required = true,
+            },
+        },
+    };
+    
+    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+}
 
 void app_main(void)
 {
   init_log();
   init_nvs();
+  init_soft_ap();
 }
