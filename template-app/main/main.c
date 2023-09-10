67cd1b5fab4dae3f198300c3bccfc77498bdf705
OlivierArgentieri <olivierargentieri@gmail.com>
2023-09-09 22:02:21 -0400

    ok very basic web server

--- template-app/main/main.c
+++ template-app/main/main.c
@@ -6,15 +6,39 @@
 #include "esp_event.h"
 #include "esp_log.h"
 #include "nvs_flash.h"
-
 #include "lwip/err.h"
 #include "lwip/sys.h"
-
+#include "esp_netif.h"
+#include "esp_http_server.h"
 
 static const char *TAG = "wifi softAP";
+static const char* html_content = "<html><body><h1>Hello, ESP32!</h1></body></html>";
+
+static esp_err_t root_handler(httpd_req_t *req) {
+    httpd_resp_send(req, html_content, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static const httpd_uri_t root_uri = {
+    .uri       = "/",
+    .method    = HTTP_GET,
+    .handler   = root_handler,
+    .user_ctx  = NULL
+};
+
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
 {
+  if (event_id == WIFI_EVENT_AP_START) {
+        esp_netif_ip_info_t ip_info;
+        esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("AP_DEF"), &ip_info);
+        
+        // Convert the IP address to a string representation
+        char ip_str[IP4ADDR_STRLEN_MAX];
+        esp_ip4addr_ntoa(&ip_info.ip, ip_str, IP4ADDR_STRLEN_MAX);
+        
+        ESP_LOGI(TAG, "SoftAP IP Address: %s", ip_str);
+    }
     if (event_id == WIFI_EVENT_AP_STACONNECTED) {
         wifi_event_ap_staconnected_t* event = (wifi_event_ap_staconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" join, AID=%d",
@@ -23,9 +47,9 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
         wifi_event_ap_stadisconnected_t* event = (wifi_event_ap_stadisconnected_t*) event_data;
         ESP_LOGI(TAG, "station "MACSTR" leave, AID=%d",
                  MAC2STR(event->mac), event->aid);
-    } 
-
+    }
 }
+
 void init_log(void)
 {
   /*
@@ -57,28 +81,20 @@ void init_nvs(void)
   }
 }
 
-static void ip_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-    ESP_LOGI(TAG, "set ip: " IPSTR, IP2STR(&(((ip_event_ap_staipassigned_t *)event_data)->ip)));
-
-}
-
 void init_soft_ap(void)
-{
+{ 
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
-
   esp_netif_create_default_wifi_ap();
+
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(
-    WIFI_EVENT,
-    ESP_EVENT_ANY_ID,
-    &wifi_event_handler,
-    NULL,
-    NULL)
-  );
-  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_AP_STAIPASSIGNED, &ip_event_handler, NULL));
+  
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        NULL));
 
 
   wifi_config_t wifi_config = {
@@ -102,11 +118,17 @@ void init_soft_ap(void)
     
     wifi_config.ap.authmode = WIFI_AUTH_OPEN;
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
 
-
+void init_http_server(void){
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  httpd_handle_t server = NULL;
+  if (httpd_start(&server, &config) == ESP_OK) {
+    httpd_register_uri_handler(server, &root_uri);
+  }
 }
 
 void app_main(void)
@@ -114,4 +136,5 @@ void app_main(void)
   init_log();
   init_nvs();
   init_soft_ap();
+  init_http_server();
 }
